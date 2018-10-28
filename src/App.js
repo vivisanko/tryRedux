b7440d29f3356197e393dfbd3c4fec07805e98ef
Victoria Sanko <sanko.victoria@gmail.com>
2018-10-29 00:04:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { updateUser } from "./actions/userActions";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+  onUpdateUser(event) {
+    this.props.onUpdateUser(event.target.value);
+  }
   render() {
     return (
       <div className="App">
@@ -19,10 +30,35 @@ class App extends Component {
           >
             Learn React
           </a>
+          <input onChange={this.onUpdateUser} />
+          <div>{this.props.user}</div>
         </header>
       </div>
     );
   }
 }
+const mapStateToProps = (state, props) => {
+  return {
+    products: state.products,
+    user: state.user,
+    userPlusProp: `${state.user} ${props.aRandomProps}`
+  };
+};
 
-export default App;
+const mapActionsToProps = (dispatch, props) => {
+  console.log("props", props);
+  return bindActionCreators({ onUpdateUser: updateUser }, dispatch);
+};
+
+// const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+//   console.log(propsFromState, propsFromDispatch, ownProps);
+//   return Object.assign({}, propsFromState, propsFromDispatch, ownProps);
+// };
+
+// console.log("mergeProps", mergeProps());
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+  // mergeProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import productsReducer from "./reducers/productsReduser";
+import userReducer from "./reducers/userReduser";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: "iPhone" }],
+    user: "Michael"
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App aRandomProps="whatever" />
+  </Provider>,
+  document.getElementById("root")
+);
+
+// console.log("state", store.getState());
+
+// const updateUserAction = {
+//   type: "updateUser",
+//   payload: {
+//     user: "John"
+//   }
+// };
+// store.dispatch(updateUserAction);
+// console.log("state", store.getState());
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
